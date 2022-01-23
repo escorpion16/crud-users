d707c351c99b1fe68d3b82581520c6f92bfe938f
Dennis Ponce <denizponce16@gmail.com>
2022-01-22 23:30:08 -0500

    Modificando estilos del componente usersList

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -2,14 +2,18 @@ import "../styles/usersList.css";
 
 const UsersList = ({ users, selectUser, deleteUser }) => {
     return (
-        <ul>
+        <ul className="users-container">
             {users.map(user => 
                 <li key={user.id} className="user-card">
-                    <div><strong>{user.first_name} {user.last_name}</strong></div>
-                    <div>{user.email}</div>
-                    <div>{user.birthday}</div>
-                    <button className="delete-btn" onClick={() => deleteUser(user)}>Delete</button>
-                    <button className="edit-btn" onClick={() => selectUser(user)}>Edit</button>
+                    <div className="card-info">
+                        <div><strong>{user.first_name} {user.last_name}</strong></div>
+                        <div>{user.email}</div>
+                        <div> <i class="fas fa-birthday-cake"></i> {user.birthday}</div>
+                    </div>
+                    <div className="card-buttons">
+                        <button className="delete-btn" onClick={() => deleteUser(user)}> <i className="fas fa-trash"></i> </button>
+                        <button className="edit-btn" onClick={() => selectUser(user)}> <i className="fas fa-edit"></i> </button>
+                    </div>
                 </li>
             )}
         </ul>
